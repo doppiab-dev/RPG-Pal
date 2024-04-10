be694eb2fd2714aa8f86296ae176d0bad361d498
Andrea <andrea.manna7@gmail.com>
2024-04-10 22:51:38 +0200

    fix: adding delete cascade

--- b4f/migrations/1712661792966_groups.js
+++ b4f/migrations/1712661792966_groups.js
@@ -4,7 +4,7 @@ exports.up = pgm => {
   pgm.createTable('groups', {
     id: { type: 'serial', primaryKey: true },
     user_id: { type: 'text', notNull: true },
-    campaign_id: { type: 'serial', references: 'campaigns(id)', notNull: true }
+    campaign_id: { type: 'serial', references: 'campaigns(id)', notNull: true, onDelete: 'cascade' }
   })
 }
 
