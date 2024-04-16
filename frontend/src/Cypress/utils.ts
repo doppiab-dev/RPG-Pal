79c7dd8c9aab2926a23fd97325c5d2324702a249
Andrea <andrea.manna7@gmail.com>
2024-04-16 18:03:44 +0200

    feat: added Component tests

--- frontend/src/Cypress/utils.ts
+++ frontend/src/Cypress/utils.ts
@@ -15,3 +15,13 @@ export const theme = createTheme({
     }
   }
 })
+export const rgbToHex = (r: number, g: number, b: number): string =>
+  "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)
+export const hexToRgb = (hex: string) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  console.log('result', result)
+
+  return result
+    ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+    : 'rgb(255, 255, 255)'
+}
