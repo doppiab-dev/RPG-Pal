69223605204965e37e119e68ce4ce8da30210861
Andrea <andrea.manna7@gmail.com>
2024-04-18 09:34:59 +0200

    chore: adding comment

--- frontend/src/Hooks/useDebouncerValidation.ts
+++ frontend/src/Hooks/useDebouncerValidation.ts
@@ -14,7 +14,8 @@ const useDebouncerValidation = (
   /**
    * Disabling react-hooks/exhaustive-deps because of this eslint error:
    * React Hook useCallback received a function whose dependencies are unknown. Pass an inline function instead.
-   * We can't pass debounce inline, because it's coming from lodash
+   * We can't pass debounce inline, because it's coming from lodash, more info at ->
+   * https://stackoverflow.com/questions/69830440/react-hook-usecallback-received-a-function-whose-dependencies-are-unknown-pass
   */
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedValidateUsername = useCallback(debounce(async (value: string) => {
