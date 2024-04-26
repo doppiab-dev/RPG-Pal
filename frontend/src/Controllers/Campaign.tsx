b22ddb0bd8349b19a04a48e9850f419ab01db242
Andrea <andrea.manna7@gmail.com>
2024-04-26 14:38:30 +0200

    feat: page skeleton

--- frontend/src/Controllers/Campaign.tsx
+++ frontend/src/Controllers/Campaign.tsx
@@ -10,22 +10,25 @@ const Campaign: FC<CampaignProps> = ({ activeCampaign }) => {
   const { t } = useTranslation()
 
   return <Stack display='flex' width='calc(100% - 250px)'>
-    <Box display='flex' width='98%' alignSelf='center' flexDirection='column'>
+    <Box display='flex' width='100%' flexDirection='column' boxShadow={1}>
       <Typography display='flex' fontSize='3rem' alignSelf='center'>{activeCampaign}</Typography>
       <Divider />
     </Box>
     <Box width='98%' alignSelf='center' flexDirection='column' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
-      <Box display='flex' width='100%' flexDirection='column' height='300px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
+      <Box display='flex' width='100%' flexDirection='column' minHeight='100px' maxHeight='300px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
         <Typography display='flex'>{t('activeCampaign.description')}</Typography>
       </Box>
       <Divider />
-      <Box display='flex' flexDirection='column' height='300px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
-        <Typography display='flex'>{t('activeCampaign.body')}</Typography>
+      <Box display='flex' flexDirection='column' minHeight='100px' maxHeight='300px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
+        <Typography display='flex'>{t('activeCampaign.campaignManagment')}</Typography>
+      </Box>
+      <Divider />
+      <Box display='flex' flexDirection='column' minHeight='100px' maxHeight='300px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
+        <Typography display='flex'>{t('activeCampaign.campaignSetting')}</Typography>
       </Box>
       <Divider />
       <Box display='flex' flexDirection='column' >
         <Typography display='flex'>{t('lorem.ipsum')}</Typography>
-        <Typography display='flex'>{t('lorem.bonus')}</Typography>
       </Box>
     </Box>
   </Stack>
--- frontend/src/Translations/en/index.ts
+++ frontend/src/Translations/en/index.ts
@@ -81,7 +81,8 @@ const userInfo = {
 }
 const activeCampaign = {
   description: 'Description con bottone',
-  body: 'body'
+  campaignManagment: 'Manage your Campaign',
+  campaignSetting: 'Manage Campaign Setting'
 }
 const lorem = {
   ipsum: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
