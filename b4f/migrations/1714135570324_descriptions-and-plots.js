46bfbc5727a45814fc99e818f0d0780e3adc9941
Andrea <andrea.manna7@gmail.com>
2024-04-26 16:22:35 +0200

    feat: create migration

---
+++ b4f/migrations/1714135570324_descriptions-and-plots.js
@@ -0,0 +1,32 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createType('places_of_interest_types', ['world', 'continent', 'state', 'region', 'area', 'city', 'camp', 'neighborhood', 'point']),
+  pgm.addColumn('campaigns', {
+    description: { type: 'text', notNull: true, default: 'to be updated' },
+    plot: { type: 'text', notNull: true, default: 'to be updated' }
+  })
+  pgm.addColumn('groups', {
+    name: { type: 'text', notNull: true, default: 'to be renamed' }
+  })
+  pgm.createTable('places_of_interest', {
+    id: { type: 'serial', primaryKey: true },
+    user_id: { type: 'text', notNull: true, references: 'users(google_id)', onDelete: 'cascade' },
+    campaign_id: { type: 'integer', references: 'campaigns(id)', notNull: true, onDelete: 'cascade' },
+    parent: { type: 'integer', notNull: false, references: 'places_of_interest(id)' },
+    children: { type: 'integer[]', notNull: false },
+    description: { type: 'text', notNull: false },
+    place: {
+      type: 'places_of_interest_types',
+      notNull: true,
+      check: "place IN ('world', 'continent', 'state', 'region', 'area', 'city', 'camp', 'neighborhood', 'point')"
+    }
+  })
+}
+
+exports.down = pgm => {
+  pgm.dropColumn('campaigns', 'description')
+  pgm.dropColumn('campaigns', 'plot')
+  pgm.dropColumn('groups', 'name')
+  pgm.dropTable('places_of_interest')
+}
