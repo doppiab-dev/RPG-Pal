2db7a649f9f20858a5fd8a5ccde89298c1b030bf
Andrea <andrea.manna7@gmail.com>
2024-05-17 16:49:36 +0200

    fix: css

--- frontend/src/Components/Description.tsx
+++ frontend/src/Components/Description.tsx
@@ -12,7 +12,15 @@ interface DescriptionProps {
 const Description: FC<DescriptionProps> = ({ description, openDescription }) => {
   const { t } = useTranslation()
 
-  return <Box display='flex' width='100%' flexDirection='column' minHeight='80px' maxHeight='250px' sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
+  return <Box
+    display='flex'
+    width='100%'
+    flexDirection='column'
+    minHeight='80px'
+    maxHeight='250px'
+    paddingTop='1vh'
+    sx={{ overflowY: 'auto', overflowX: 'hidden' }}
+  >
     <Typography variant="h6" component="h2">{t('activeCampaign.description')}</Typography>
     <Text
       emptyText={t('activeCampaign.noDescription')}
