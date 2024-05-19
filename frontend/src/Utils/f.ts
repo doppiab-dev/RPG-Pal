d98a1ca891dd195046744800253b7989b4e0327e
Andrea <andrea.manna7@gmail.com>
2024-05-19 12:25:36 +0200

    fix: address sonarcloud changes

--- frontend/src/Utils/f.ts
+++ frontend/src/Utils/f.ts
@@ -138,4 +138,4 @@ export const removeHtmlTags = (text: string): string => {
 
 // TODO: per avere b,u,i segui: https://github.com/diegomura/react-pdf/issues/164#issuecomment-434641415
 export const cleanStyle = (text: string): string =>
-  text.replace(/<\/?(b|u|i)>/g, '')
+  text.replace(/<\/?([bui])>/g, '')
